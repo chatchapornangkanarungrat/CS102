5277db55fd95cedf18b12b4a618448a8c6a5d004
Chatchaporn Angkanarungrat <chatchaporn.ang@dome.tu.ac.th>
2024-11-24 14:15:14 +0700

    Correct the worked of hw 4-1

--- HW4 cs102/cs102Hw4-1-6709616418.c
+++ HW4 cs102/cs102Hw4-1-6709616418.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
 
-void swapping (int n[100], int x, int y) {
-	int z;
-	z=n[x];
-	n[x]=n[y];
-	n[y]=z;
+void swap_elements(int *arr, int pos1, int pos2) {
+    int z;
+	z= arr[pos1];
+    arr[pos1] = arr[pos2];
+    arr[pos2] = z;
 }
 
-int main () {
-	int x,y,i,e;
-	int n[100];
-	
-	printf("Enter the number of elements (max 100): ");
-	scanf("%d", &e);
-	
-	printf("Enter %d numbers: ", e);
-	for(i=0;i<e;i++)
-	scanf("%d", &n[i]);
-	
-	printf("Enter first position to swap (0-%d): ", e-1);
-	scanf("%d", &x);
-	printf("Enter second position to swap (0-%d): ", e-1);
-	scanf("%d", &y);
-	
-	swapping (n,x,y);
-	printf("After swapping elements at positions %d and %d: \n", x,y);
-	for(i=0;i<e;i++)
-	printf("%d ", n[i]);
-	
-return 0;
+int main() 
+{
+    int n[100];
+    int e, pos1, pos2,i;
+
+    printf("Enter the number of elements (max 100): ");
+    scanf("%d", &e);
+
+
+    printf("Enter %d numbers: ", e);
+    for (i=0;i<e;i++) 
+        scanf("%d", &n[i]);
+
+    printf("Enter first position to swap (0-%d): ", e-1);
+    scanf("%d", &pos1);
+    printf("Enter second position to swap (0-%d): ", e-1);
+    scanf("%d", &pos2);
+
+    swap_elements(n, pos1, pos2);
+    printf("After swapping elements at positions %d and %d:\n", pos1, pos2);
+    for (i=0;i<e;i++) 
+        printf("%d ", n[i]);
+
+    return 0;
 }
